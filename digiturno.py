82e1078be7ac3de64b4a652eca1d056559bab532
Yefrit1 <y_perez14@unisimon.edu.co>
2025-03-18 11:40:45 -0500

    Added Asociado layout

--- digiturno.py
+++ digiturno.py
@@ -19,19 +19,112 @@ class MainWindow(QMainWindow):
 ####### Layout ENTRADA #######
         self.ent = QWidget()
         self.layoutEnt = QVBoxLayout(self.ent)
-        self.layoutEnt.setAlignment(Qt.AlignTop | Qt.AlignCenter)
-        bluleibel = QLabel("ENTRADA")
-        boton = QPushButton("testststet")
-        boton.setFixedSize(int(self.screenGeometry.width()*0.2), int(self.screenGeometry.height()*0.2))
-        self.layoutEnt.addWidget(bluleibel)
-        self.layoutEnt.addWidget(boton)
+        self.layoutEnt.setAlignment(Qt.AlignTop)
+        self.layoutEnt.setSpacing(self.screen_height(15))
+        self.ent.setLayout(self.layoutEnt)
+        #
+        self.entHbox1 = QHBoxLayout()
+        self.entHbox1.setSpacing(5)
+
+        bienvenido = QLabel("Bienvenido")
+        self.style_label(bienvenido, 5.5)
+
+        spacer1 = QLabel() # Spacing
+        
+        self.add_logo(self.entHbox1)
+        self.entHbox1.addWidget(bienvenido)
+        self.entHbox1.addWidget(spacer1)
+        #
+        pregunta = QLabel("¿Eres asociado?")
+        self.style_label(pregunta, 9)
+        #
+        self.entHbox2 = QHBoxLayout()
+        self.entHbox2.setAlignment(Qt.AlignCenter)
+        self.entHbox2.setSpacing(self.screen_width(10))
+
+        self.entNo = QPushButton("No")
+        self.entSi = QPushButton("Sí")
+        self.entNo.setFixedWidth(self.screen_width(20))
+        self.entSi.setFixedWidth(self.screen_width(20))
+        self.style_button(self.entNo, 10, 30, 2)
+        self.style_button(self.entSi, 10, 30, 2)
+        self.entNo.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(2))
+        self.entSi.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(1))
+
+        self.entHbox2.addWidget(self.entNo)
+        self.entHbox2.addWidget(self.entSi)
+        #
+        self.layoutEnt.addLayout(self.entHbox1)
+        self.layoutEnt.addWidget(pregunta)
+        self.layoutEnt.addLayout(self.entHbox2)
 
 ####### Layout ASOCIADO #######
         self.asc = QWidget()
         self.layoutAsc = QVBoxLayout(self.asc)
-        self.layoutAsc.setAlignment(Qt.AlignTop | Qt.AlignCenter)
-        asos = QLabel("ASOCIADO")
-        self.layoutAsc.addWidget(asos)
+        self.layoutAsc.setAlignment(Qt.AlignTop)
+        #
+        self.ascHbox1 = QHBoxLayout()
+        self.ascHbox1.setContentsMargins(0, 0, 0, self.screen_height(10))
+
+        labelCedula = QLabel("Ingresa tu cédula")
+        self.style_label(labelCedula, 5)
+
+        spacer2 = QLabel() # Spacing
+
+        self.add_logo(self.ascHbox1)
+        self.ascHbox1.addWidget(labelCedula)
+        self.ascHbox1.addWidget(spacer2)
+        #
+        self.ascHbox2 = QHBoxLayout()
+        self.ascHbox2.setAlignment(Qt.AlignCenter)
+
+        self.cedula = QLineEdit()
+        self.cedula.setFixedWidth(self.screen_width(60))
+        self.style_line_edit(self.cedula, 8)
+        self.cedula.setReadOnly(True)
+
+        self.ascHbox2.addWidget(self.cedula)
+        #
+        self.ascHbox3 = QHBoxLayout()
+
+        volver = QPushButton("Volver")
+        volver.setFixedWidth(self.screen_width(17))
+        self.style_button(volver, 5, 15, 2)
+
+        spacer3 = QLabel()
+
+        self.keypad = QGridLayout()
+        self.keypad.setAlignment(Qt.AlignCenter)
+        self.keypad.setSpacing(0)
+        kpadButtons = [('7',0,0), ('8',0,1), ('9',0,2),
+                        ('4',1,0), ('5',1,1), ('6',1,2),
+                        ('1',2,0), ('2',2,1), ('3',2,2),
+                        ('Borrar',3,0), ('0',3,1), ('',3,2)]
+        for text,row,col in kpadButtons:
+            kButton = QPushButton(text)
+            kButton.setFixedWidth(self.screen_width(10))
+            if kButton.text() == "Borrar":
+                self.style_button(kButton, 2, 15, 2)
+                kButton.setFixedHeight(self.keypad.itemAtPosition(0,0).geometry().height())
+                print(f"borrar size: {self.keypad.itemAtPosition(0,0).geometry().height()}")
+            else: self.style_button(kButton, 5, 15, 2)
+            kButton.clicked.connect(self.kpad_pressed)
+            print(f"num size: {kButton.height()}")
+            self.keypad.addWidget(kButton, row, col)
+        
+        spacer4 = QLabel()
+        spacer5 = QLabel()
+        spacer5.setFixedWidth(volver.width())
+
+        self.ascHbox3.addWidget(volver)
+        self.ascHbox3.addWidget(spacer3)
+        self.ascHbox3.addLayout(self.keypad)
+        self.ascHbox3.addWidget(spacer4)
+        self.ascHbox3.addWidget(spacer5)
+        #
+        self.layoutAsc.addLayout(self.ascHbox1)
+        self.layoutAsc.addLayout(self.ascHbox2)
+        self.layoutAsc.addLayout(self.ascHbox3)
         
 ####### Layout NO ASOCIADO #######
         self.noAsc = QWidget()
@@ -47,33 +140,73 @@ class MainWindow(QMainWindow):
         self.stackedWidget.addWidget(self.noAsc)
         self.stackedWidget.addWidget(self.serv)
 
-        # Navigation buttons
-        self.create_navigation_buttons(main_layout)
         self.showFullScreen()
 
-    def create_navigation_buttons(self, main_layout):
-        nav_layout = QHBoxLayout()
+    # Called when a button from the keypad is pressed
+    def kpad_pressed(self):
+        button = self.sender()
+        current_number = self.cedula.text()
+        new_number = current_number + button.text()
+        self.cedula.setText(new_number)
+    
+    # Sets stylesheet for a label
+    def style_label(self, label, fontSize):
+        label.setStyleSheet(f"""
+            QLabel {{
+                color: #284F1A;
+                font-size: {self.screen_width(fontSize)}px;
+            }}
+        """)
+        shadow = QGraphicsDropShadowEffect(label, blurRadius=20)
+        shadow.setOffset(5, 5)
+        label.setGraphicsEffect(shadow)
+        label.setAlignment(Qt.AlignCenter)
 
-        # Create buttons for each layout
-        button1 = QPushButton("Go to ENTRADA")
-        button2 = QPushButton("Go to ASOCIADO")
-        button3 = QPushButton("Go to NO ASOCIADO")
-        button4 = QPushButton("Go to SERVICIO")
+    # Sets stylesheet for a button
+    def style_button(self, button, fontSize, radius, border):
+        button.setStyleSheet(f"""
+            QPushButton {{
+                background: qlineargradient(
+                    x1:0.5, y1:0, x2:0.5, y2:1,
+                    stop:0 #85A947, stop:1 #3E7B27
+                );
+                border-radius: {radius}px;
+                border: {border}px solid black;
+                color: white;
+                font-size: {self.screen_width(fontSize)}px;
+            }}
+            QPushButton:pressed{{
+                background: qlineargradient(
+                    x1:0.5, y1:0, x2:0.5, y2:1,
+                    stop:0 #A9DB55, stop:1 #85A947
+                )
+            }}
+        """)
+        shadow = QGraphicsDropShadowEffect(button, blurRadius=20)
+        shadow.setOffset(5, 5)
+        button.setGraphicsEffect(shadow)
 
-        # Connect buttons to switch layouts
-        button1.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(0))
-        button2.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(1))
-        button3.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(2))
-        button4.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(3))
+    # Sets stylesheet for a line edit
+    def style_line_edit(self, label, fontSize):
+        label.setStyleSheet(f"""
+            QLineEdit {{
+                color: #204114;
+                border-radius:10px;
+                font-size: {self.screen_width(fontSize)}px;
+            }}
+        """)
+        label.setAlignment(Qt.AlignCenter)
 
-        # Add buttons to the navigation layout
-        nav_layout.addWidget(button1)
-        nav_layout.addWidget(button2)
-        nav_layout.addWidget(button3)
-        nav_layout.addWidget(button4)
-
-        # Add the navigation layout to the main layout
-        main_layout.addLayout(nav_layout)
+    # Adds a logo label
+    def add_logo(self, layout):
+        label = QLabel()
+        label.setAlignment(Qt.AlignTop)
+        pixmap = QPixmap("logoCoohem.png")
+        label.setPixmap(pixmap)
+        shadow = QGraphicsDropShadowEffect(label, blurRadius=20)
+        shadow.setOffset(5, 5)
+        label.setGraphicsEffect(shadow)
+        layout.addWidget(label)
 
     # Used on a widget to set background color
     def set_background_color(self, widget, color):
@@ -91,8 +224,14 @@ class MainWindow(QMainWindow):
             palette.setBrush(QPalette.Window, brush)
             widget.setAutoFillBackground(True)
             widget.setPalette(palette)
-
-
+    
+    # Returns pixel value of screen width % based on parameter
+    def screen_width(self, num):
+        return int(self.screenGeometry.width()*num/100)
+    # Returns pixel value of screen height % based on parameter
+    def screen_height(self, num):
+        return int(self.screenGeometry.height()*num/100)
+    
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
